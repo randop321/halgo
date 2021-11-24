305f6770a557762396e0427af4590b4cc930e410
randop321 <randop321@outlook.com>
2021-11-24 23:07:40 +0800

    add pollard's rho algo

--- number-theory/prime.hs
+++ number-theory/prime.hs
@@ -4,6 +4,9 @@
     
     isPrime: Miller Rabin Test to 30 tests 
     isPrime n s: test whether n is prime or not with random seed s
+    
+    primeFactoring: prime factoring and return a list of factors using Pollard's Rho Algorithm
+    primeFactoring n s: factor n with random seed s
 -}
 
 
@@ -55,3 +58,40 @@ isPrime n seed
         init_val = (1, mkStdGen seed)
         randomlist = map (\x -> (mod (fromIntegral $ fst x) (n-1))+1) $ scanl (\a x -> next $ snd a) init_val [1..30]
         
+        
+        
+-- Pollard's Rho Algorithm
+-- assuming n is not prime
+pollardsRhoSeq :: (Integral a) => a -> a -> a -> a -> a
+pollardsRhoSeq n x y k
+    | g == 1 = pollardsRhoSeq n xx yy k
+    | g == n = n
+    | otherwise = g
+    where
+        g = gcd (abs (xx-yy))  n
+        xx = mod (x*x+k) n
+        y1 = mod (y*y+k) n
+        yy = mod (y1*y1+k) n
+
+pollardsRho :: (Integral a) => a -> StdGen -> a
+pollardsRho n stdgen
+    | (mod n 2) == 0 = 2
+    | g == n = pollardsRho n c
+    | otherwise = g
+    where
+        g = pollardsRhoSeq n m m k
+        a = next stdgen
+        b = next $ snd a
+        c = snd b
+        m = fromIntegral $ fst a
+        k = fromIntegral $ fst b
+        
+primeFactoring :: (Integral a) => a -> Int -> [a]
+primeFactoring n seed
+    | n < 0 = -1 : (primeFactoring (-n) seed)
+    | n <=1 = []
+    | isPrime n seed = [n]
+    | otherwise = (primeFactoring a seed) ++ (primeFactoring (div n a) seed)
+    where
+        a = pollardsRho n $ mkStdGen seed
+
